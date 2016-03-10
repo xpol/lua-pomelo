c918a3d3aed1ee3ccf427c27580eeadcd962ddbd
Xpol Wan <xpolife@gmail.com>
2016-03-10 22:10:23 +0800

    the runner pass -o plainTerminal to busted and accept command args.
    ... and add a c test entry for testing.

--- spec/runner.c
+++ spec/runner.c
@@ -43,6 +43,21 @@ void preload_lua_modules(lua_State *L)
     lua_pop(L, 2);
 }
 
+static void set_arg(lua_State* L, int argc, char **argv)
+{
+    int i, n;
+    lua_createtable(L, 0, 0);
+    for (i = 2, n = 1; i < argc; ++i, ++n) {
+        lua_pushstring(L, argv[i]);
+        lua_rawseti(L, -2, n);
+    }
+    lua_pushliteral(L, "-o");
+    lua_rawseti(L, -2, n++);
+    lua_pushliteral(L, "plainTerminal");
+    lua_rawseti(L, -2, n++);
+
+    lua_setglobal(L, "arg");
+}
 
 // runner spec/xxx_spec.lua [bustedoptions]
 static int run(int argc, char **argv)
@@ -50,15 +65,30 @@ static int run(int argc, char **argv)
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
     preload_lua_modules(L);
-    lua_createtable(L, 0, 0);
-    lua_setglobal(L, "arg");
+    set_arg(L, argc, argv);
     if (luaL_dofile(L, argc > 1 ? argv[1] : PROJECT_ROOT "/spec/pomelo_spec.lua") != 0)
         traceback(L);
     lua_close(L);
     return 0;
 }
 
+#include "pomelo.h"
+
+static int runc()
+{
+    pc_client_t* client = (pc_client_t*)malloc(pc_client_size());
+    pc_client_config_t config = PC_CLIENT_CONFIG_DEFAULT;
+    config.enable_polling = 1;
+    //pc_lib_init(NULL, NULL, NULL, "Test Client");
+    pc_client_init(client, NULL, &config);
+    pc_client_connect(client, "127.0.0.1", 3010, NULL);
+    pc_client_disconnect(client);
+    pc_client_cleanup(client);
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    return run(argc, argv);
+    run(argc, argv);
+    return runc();
 }
